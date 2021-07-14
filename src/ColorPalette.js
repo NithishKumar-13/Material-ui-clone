3f11d9e94c5d1dee601c1b8e58f4ac6d96a434c7
Nithish Kumar <nithishpadma@gmail.com>
2021-07-14 11:07:20 +0530

    create ColorPalette.js and add colors

---
+++ src/ColorPalette.js
@@ -0,0 +1,24 @@
+const colorPalettes = [
+    { id:1 , colorName:'red' , defaultOpacity: 500 , colorCode: '#F44336'},
+    { id:2 , colorName:'pink' , defaultOpacity: 500 , colorCode: '#E91E63'},
+    { id:3 , colorName:'purple' , defaultOpacity: 500 , colorCode: '#9C27B0'},
+    { id:4 , colorName:'deep purple' , defaultOpacity: 500 , colorCode: '#673AB7'},
+    { id:5 , colorName:'indigo' , defaultOpacity: 500 , colorCode: '#3F51B5'},
+    { id:6 , colorName:'blue' , defaultOpacity: 500 , colorCode: '#2196F3'},
+    { id:7 , colorName:'light blue' , defaultOpacity: 500 , colorCode: '#03A9F4'},
+    { id:8 , colorName:'cyan' , defaultOpacity: 500 , colorCode: '#00BCD4'},
+    { id:9 , colorName:'teal' , defaultOpacity: 500 , colorCode: '#009688'},
+    { id:10 , colorName:'green' , defaultOpacity: 500 , colorCode: '#4CAF50'},
+    { id:11 , colorName:'light green' , defaultOpacity: 500 , colorCode: '#8BC34A'},
+    { id:12 , colorName:'lime' , defaultOpacity: 500 , colorCode: '#CDDC39'},
+    { id:13 , colorName:'yellow' , defaultOpacity: 500 , colorCode: '#FFEB3B'},
+    { id:14 , colorName:'amber' , defaultOpacity: 500 , colorCode: '#FFC107'},
+    { id:15 , colorName:'orange' , defaultOpacity: 500 , colorCode: '#FF9800'},
+    { id:16 , colorName:'deep orange' , defaultOpacity: 500 , colorCode: '#FF5722'},
+    { id:17 , colorName:'brown' , defaultOpacity: 500 , colorCode: '#795548'},
+    { id:18 , colorName:'grey' , defaultOpacity: 500 , colorCode: '#9E9E9E'},
+    { id:19 , colorName:'blue grey' , defaultOpacity: 500 , colorCode: '#607D8B'},
+    { id:20 , colorName:'black' , defaultOpacity: 500 , colorCode: '#111'},
+]
+
+export default colorPalettes
