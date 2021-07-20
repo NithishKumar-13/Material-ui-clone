a217d17a6241e5198e129daa55e1aa50e699a30e
Nithish Kumar <nithishpadma@gmail.com>
2021-07-20 21:25:35 +0530

    setup App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React  from 'react'
+import './App.css';
+import Home from './components/Home/Home'
+import { Route , Switch } from 'react-router-dom'
+import GenerateScale from './components/GenerateScale/GenerateScale'
 
-const App = () => {
-    return(
-        <div>
-            App
-        </div>
-    )
+function App() {
+ 
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/generateScale/:colorName' component={GenerateScale} />
+      </Switch>
+    </div>
+  );
 }
 
-export default App
+export default App;
